fc658880789bb01c36d3fa2d0d199e54149a86ff
Gomuro <75692732+Gomuro@users.noreply.github.com>
2021-03-04 14:34:54 +0200

    New

--- app/src/main/java/com/example/oplop/MainActivity.java
+++ app/src/main/java/com/example/oplop/MainActivity.java
@@ -29,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
                 
             }
         });
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(getClass().getName(), "Value is:" + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
     }
 }
